1c9fa4da9e7a22505b2c573119c352cc4f9fe838
MarvinAchas <marvinacha@ghlperu.com>
2025-08-18 17:36:43 -0500

    first commit

---
+++ src/main/java/code_talent_gallery_back/com/CodeTalentGalleryBackApplication.java
@@ -0,0 +1,13 @@
+package code_talent_gallery_back.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTalentGalleryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTalentGalleryBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code_talent_gallery_back/com/CodeTalentGalleryBackApplicationTests.java
@@ -0,0 +1,13 @@
+package code_talent_gallery_back.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTalentGalleryBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
